de2b02879e086f5c55815d1089fdc0d38470659e
root <root@m0roz.nikitta.hlab.kz>
2026-08-21 09:40:46 +0500

    calc dev

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["176.12.73.205"],
   images: {
     remotePatterns: [new URL("https://xn--b1adlqilgi.xn--p1ai/**")],
   },
